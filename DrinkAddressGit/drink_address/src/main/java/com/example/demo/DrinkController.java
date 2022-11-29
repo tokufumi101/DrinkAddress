bfe7917895dcc1649ee6379bf950cb24b1beb0a8
tokufumi101 <kubrik244@gmail.com>
2022-11-29 21:33:18 +0900

    refactor:Controllerの記述をServiceに分ける

--- DrinkAddressGit/drink_address/src/main/java/com/example/demo/DrinkController.java
+++ DrinkAddressGit/drink_address/src/main/java/com/example/demo/DrinkController.java
@@ -1,12 +1,5 @@
 package com.example.demo;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +16,7 @@ import com.example.demo.dao.DrinkDao;
 import com.example.demo.dto.DrinkDto;
 import com.example.demo.entity.AddressEnt;
 import com.example.demo.entity.DrinkEnt;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.demo.service.DrinkService;
 
 @Controller
 public class DrinkController {
@@ -33,103 +25,26 @@ public class DrinkController {
 	DrinkDao drinkRepository;
 	@Autowired
 	AddressDao addressRepository;
+	@Autowired
+	DrinkService drinkService;
 
 	@GetMapping("/top")
 	public String top(Model model, AddressEnt addressEnt) throws Exception {
-		model.addAttribute("message", "ようこそ");
 		List list = drinkRepository.findAll();
 		model.addAttribute("data", list);
 		List addressList = addressRepository.findAll();
 		model.addAttribute("tableData", addressList);
-
-
-		
 		return "top";
 	}
 
 	@PostMapping("/top")
-	public String add(@RequestParam("name") String name, Model model, DrinkEnt drinkEnt, AddressEnt addressEnt,
+	public String add(@RequestParam("name") String name, DrinkEnt drinkEnt, AddressEnt addressEnt,
 			@ModelAttribute DrinkDto drinkDto, RedirectAttributes redirectAttributes) {
 
-		// 飲み物リストをプルダウンで表示
-		List list = drinkRepository.findAll();
-		model.addAttribute("data", list);
-		model.addAttribute("message", "登録完了しました");
-
-		// ドリンクリストに新しいドリンクの登録
-		if (drinkRepository.existsByName(name) == false) {
-			DrinkEnt drinkPlus = new DrinkEnt(name);
-			System.out.println(name);
-
-			drinkPlus.setId(list.size() + 1);
-			drinkRepository.saveAndFlush(drinkPlus);
-		}
-
-		addressEnt.setAddress(drinkDto.getAddress());
-
-		// timestampを入れる
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		addressEnt.setRegisterDate(timestamp);
-
-		DrinkEnt drink = drinkRepository.findByName(name);
-
-		addressEnt.setDrinkEnt(drink);
-		System.out.println(addressEnt.getDrinkEnt().getName());
-
-		// データがそろったのでテーブルに登録
-
-		String address = addressEnt.getAddress();
-		String makeUrl = "https://msearch.gsi.go.jp/address-search/AddressSearch?q=";
-		String sQuote = null;
-		String result = "";
-		JsonNode root = null;
-		try {
-			sQuote = URLEncoder.encode(address, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		String a = makeUrl + sQuote;
-		System.out.println(a);
-		try {
-			URL url = new URL(a);
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.connect(); // URL接続
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String tmp = "";
-			while ((tmp = in.readLine()) != null) {
-				result += tmp;
-			}
-			ObjectMapper mapper = new ObjectMapper();
-			root = mapper.readTree(result);
-			in.close();
-			con.disconnect();
-			System.out.println(root);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-//		System.out.println(root);
-//		 for( JsonNode reqgeo : root) {
-//	        	System.out.println(reqgeo.get("geometry").get("coordinates")); //緯度経度取得
-//	        	
-//	        }
-//		 addressEnt.setLatitude();
-		try {
-		double lat = root.get(0).get("geometry").get("coordinates").get(0).asDouble();
-		double lon = root.get(0).get("geometry").get("coordinates").get(1).asDouble();
-		addressEnt.setLatitude(lat);
-		addressEnt.setLongitude(lon);
-		addressRepository.saveAndFlush(addressEnt);
-		}catch(NullPointerException e) {
-			e.printStackTrace();
-			redirectAttributes.addFlashAttribute("flashmsg", "入力値が無効です");
-			return "redirect:/top";
-		}
+		String message=(!drinkService.register(name, drinkEnt, addressEnt, drinkDto))
+				? "入力値が無効です":"登録完了しました";
 		
-
-		List addressList = addressRepository.findAll();
-
-		model.addAttribute("tableData", addressList);
-		redirectAttributes.addFlashAttribute("flashmsg", "登録完了しました");
+		redirectAttributes.addFlashAttribute("flashmsg", message);
 		
 		return "redirect:/top";
 	}
